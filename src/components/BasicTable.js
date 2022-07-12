f485fcdec765f4c1d4c5fd95edb172270f70c847
fahad <fahad.baig@costrategix.com>
2022-07-12 13:30:32 +0530

    checked

---
+++ src/components/BasicTable.js
@@ -0,0 +1,38 @@
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./columns";
+
+const BasicTable = (props) => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const tableInstance = useTable({
+    columns,
+    data,
+  });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  return (
+    <div>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((columns) => (
+                <th {...columns.getHeaderProps()}>
+                  {columns.render("Headers")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}></tbody>
+      </table>
+    </div>
+  );
+};
+
+export default BasicTable;
