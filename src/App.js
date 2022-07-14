4cca6b3a3b12e3293bfb9be3552355ee641bdb6c
fahad <fahad.baig@costrategix.com>
2022-07-14 19:37:46 +0530

    added column hiding feature.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 // import { BasicTable } from "./components/BasicTable";
 // import { SortingTable } from "./components/SortingTable";
 // import { FilteringTable } from "./components/FilteringTable";
-import { PaginationTable } from "./components/PaginationTable";
+// import { PaginationTable } from "./components/PaginationTable";
+// import { RowSelection } from "./components/RowSelection";
+import { ColumnHiding } from "./components/ColumnHiding";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <PaginationTable />
+    <div>
+      <ColumnHiding />
     </div>
   );
 }
---
+++ src/components/RowSelection.jsx
@@ -0,0 +1,104 @@
+import React, { useMemo } from "react";
+import { useTable, useRowSelect } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS /* GROUPED_COLUMNS */ } from "./columns";
+import "./table.css";
+import { Checkbox } from "./Checkbox";
+
+export const RowSelection = (props) => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+    selectedFlatRows,
+    state: { selectedRowIds },
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useRowSelect,
+    (hooks) => {
+      hooks.visibleColumns.push((columns) => [
+        // Let's make a column for selection
+        {
+          id: "selection",
+          // The header can use the table's getToggleAllRowsSelectedProps method
+          // to render a checkbox
+          Header: ({ getToggleAllRowsSelectedProps }) => (
+            <div>
+              <Checkbox {...getToggleAllRowsSelectedProps()} />
+            </div>
+          ),
+          // The cell can use the individual row's getToggleRowSelectedProps method
+          // to the render a checkbox
+          Cell: ({ row }) => (
+            <div>
+              <Checkbox {...row.getToggleRowSelectedProps()} />
+            </div>
+          ),
+        },
+        ...columns,
+      ]);
+    }
+  );
+
+  return (
+    <div>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.slice(0, 10).map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map((footerGroup) => (
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map((column) => (
+                <td {...column.getFooterProps}> {column.render("Footer")} </td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              selectedRowIds: selectedRowIds,
+              "selectedFlatRows[].original": selectedFlatRows.map(
+                (d) => d.original
+              ),
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
+    </div>
+  );
+};
